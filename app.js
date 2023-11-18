177af96791c0c973ca26ef6b03c0ebff5f1d7e21
stevenliz0808 <tw3638993@gmail.com>
2023-11-18 13:53:41 +0800

    Add: create-test & start-test pages

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-const express = require("express")
-const app = express()
-const {
-    engine
-} = require('express-handlebars')
+const express = require("express");
+const app = express();
+const { engine } = require("express-handlebars");
 
-const port = 3000
+const port = 3000;
 
-app.engine('.hbs', engine({
-    extname: '.hbs'
-}))
-app.set('view engine', '.hbs');
-app.set('views', './views');
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.get("/test/create", (req, res) => {
+  res.render("create-test");
+});
+
+app.get("/test/start", (req, res) => {
+  res.render("start-test");
+});
+
+app.get("/test", (req, res) => {
+  res.render("index");
+});
+
+app.get("/", (req, res) => {
+  res.redirect("/test");
+});
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
